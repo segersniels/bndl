e5d88e182f2df45e5ec457c6959bed8a9e257995
segersniels <segers.n@hotmail.com>
2023-12-01 12:38:05 +0100

    :ambulance: Make sure we follow the symlink correctly

--- scripts/run.sh
+++ scripts/run.sh
@@ -23,4 +23,11 @@ Darwin)
     ;;
 esac
 
-exec ./bin/$PLATFORM_BINARY "$@"
+# Determine where on the filesystem the script is located, since it is most likely symlinked
+LOCATION_ON_FILE_SYSTEM=$(dirname $([ -L $0 ] && readlink -f $0 || echo $0))
+EXEC_BINARY=$LOCATION_ON_FILE_SYSTEM/../bin/$PLATFORM_BINARY
+
+# Ensure the binary is executable
+chmod +x $EXEC_BINARY
+
+exec $EXEC_BINARY "$@"
